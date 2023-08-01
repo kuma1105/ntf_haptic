d68ce2390546b496cd76574a32a32439503607f3
Fancyee <74527909+kuma1105@users.noreply.github.com>
2023-08-01 14:24:07 +0900

    진동 4단계(수정)

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,29 +48,41 @@ class _MyHomePageState extends State<MyHomePage> {
               width: 200,
               height: 100,
               child: ElevatedButton(onPressed: () {
-                Vibration.vibrate(duration: 1000, amplitude: 50);
-              }, child: Text("duration: 1000, amplitude: 50")),
+                Vibration.vibrate(pattern: [1000, 500, 1000, 500, 1000, 500],
+                    intensities: [1, 50, 1, 50, 1, 50]);
+              }, child: Text("duration: 500ms, amplitude: 50, interval: 1000ms")),
             ),
             Container(
               width: 200,
               height: 100,
               child: ElevatedButton(onPressed: () {
-                Vibration.vibrate(duration: 1000, amplitude: 100);
-              }, child: Text("duration: 1000, amplitude: 100")),
+                Vibration.vibrate(pattern: [750, 500, 750, 500, 750, 500],
+                    intensities: [1, 100, 1, 100, 1, 100]);
+              }, child: Text("duration: 500, amplitude: 100, interval: 750ms")),
             ),
             Container(
               width: 200,
               height: 100,
               child: ElevatedButton(onPressed: () {
-                Vibration.vibrate(duration: 1000, amplitude: 150);
-              }, child: Text("duration: 1000, amplitude: 150")),
+                Vibration.vibrate(pattern: [500, 500, 500, 500, 500, 500],
+                    intensities: [1, 150, 1, 150, 1, 150]);
+              }, child: Text("duration: 500, amplitude: 150, interval: 500ms")),
             ),
             Container(
               width: 200,
               height: 100,
               child: ElevatedButton(onPressed: () {
-                Vibration.vibrate(duration: 1000, amplitude: 200);
-              }, child: Text("duration: 1000, amplitude: 200")),
+                Vibration.vibrate(pattern: [250, 500, 250, 500, 250, 500],
+                    intensities: [1, 200, 1, 200, 1, 200]);
+              }, child: Text("duration: 500, amplitude: 200, interval: 250ms")),
+            ),
+            Container(
+              width: 200,
+              height: 100,
+              child: ElevatedButton(onPressed: () {
+                // Vibration.vibrate(duration: 1000, amplitude: 200);
+                Vibration.cancel();
+              }, child: Text("Cancel the vibration")),
             )
           ],
         ),
